26eaec1026f90008b45da98ca33416af3b53193d
Thomas Thomassen <thomas@thomthom.net>
2013-11-04 12:59:34 +0100

    Corrected method "world" to "hello_world".

--- Hello World/src/SUEX_HelloWorld.cpp
+++ Hello World/src/SUEX_HelloWorld.cpp
@@ -13,6 +13,6 @@ void Init_SUEX_HelloWorld()
 {
   VALUE mSUEX_HelloWorld = rb_define_module("SUEX_HelloWorld");
   rb_define_const(mSUEX_HelloWorld, "CEXT_VERSION", GetRubyInterface("1.0.0"));
-  rb_define_module_function(mSUEX_HelloWorld, "hello",
+  rb_define_module_function(mSUEX_HelloWorld, "hello_world",
     VALUEFUNC(hello_world), 0); 
 }
